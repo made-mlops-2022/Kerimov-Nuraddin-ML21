1beb8520b28ab31cc1a7f1a999ef4153af555a51
kaizer-nurik <kerimovnm@mail.ru>
2022-11-06 22:58:50 +0300

    ===

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='HW1 for MLops',
+    author='Kerimov Nuraddin ML-21',
+    license='MIT',
+)
