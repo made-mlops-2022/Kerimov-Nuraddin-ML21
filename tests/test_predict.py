e3cb2620e568367d96116eb68953eea409ae8fa6
kaizer-nurik <kerimovnm@mail.ru>
2022-11-20 18:29:17 +0300

    Update test_predict.py

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -3,9 +3,10 @@ import os
 from ml_example.predict import predict
 from ml_example.data.make_dataset import generete_fake_data
 
+
 class TestTrain(unittest.TestCase):
     def test_train(self):
-        generete_fake_data('./tests/data/raw/fake_data.csv',1000)
+        generete_fake_data('./tests/data/raw/fake_data.csv', 1000)
         predict('./tests/data/raw/fake_data.csv',
                 './models/model.pkl', './tests/res.csv')
         self.assertTrue(os.path.exists('./tests/res.csv'))
